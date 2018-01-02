f436802062301c5a68879913a2a1e37984cd616c
Shikang Du <skdu.dusk@gmail.com>
2018-01-02 21:15:22 +0800

    add support for MinGW; fix typos

--- init_threads.cpp
+++ init_threads.cpp
@@ -8,7 +8,7 @@ void init_threads()
     if(0 != pthread_create(&thread_dialog, NULL, &do_dialog, NULL))
         throw "pthread_create(thread_dialog) Failed.";
     if(0 != pthread_create(&thread_recognize, NULL, &do_recognize, NULL))
-        throw "pthread_create(thread_dialog) Failed.";
+        throw "pthread_create(thread_recognize) Failed.";
     if(0 != pthread_create(&thread_generate, NULL, &do_generate, NULL))
-        throw "pthread_create(thread_dialog) Failed.";
+        throw "pthread_create(thread_generate) Failed.";
 }
